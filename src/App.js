4b202cde441a51d8a903f9f2f245e9ed7a0a5952
Peter Gemmell <petergemmell65@hotmail.com>
2020-11-30 15:16:07 +0000

    form created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import {APP_ID} from './config'
+import {APP_KEY} from './config'
 
 const App = () => {
 
   const APP_ID = '';
+  const APP_KEY = '';
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   return(
     <div className="App">
-    <h1> Hello React!!</h1>
+    <form>
+    <input type="text"/>
+    <button></button>
+    </form>
     </div>
   );
 }
