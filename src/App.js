7d30d2b6a7abb1f8b702b9d2cd2fcb927531c95e
Peter Gemmell <petergemmell65@hotmail.com>
2020-11-30 15:53:02 +0000

    working on cors issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import logo from './logo.svg';
+import React, { useEffect, useState} from "react";
 import './App.css';
 import {APP_ID} from './config'
 import {APP_KEY} from './config'
@@ -9,13 +8,22 @@ const App = () => {
   const APP_ID = '';
   const APP_KEY = '';
 
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      console.log(data);
+  }
 
   return(
     <div className="App">
-    <form>
-    <input type="text"/>
-    <button></button>
+    <form className = "search-form">
+    <input className="search-bar" type="text"/>
+    <button className="search-button" type="submit">Search</button>
     </form>
     </div>
   );
