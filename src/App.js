682a3c35febe4e90d58c517715275aff66c339ec
Peter Gemmell <petergemmell65@hotmail.com>
2020-11-30 15:02:11 +0000

    further changes made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import {APP_ID} from './config'
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '';
+
+  return(
     <div className="App">
-      <h1> Hello React</h1>
+    <h1> Hello React!!</h1>
     </div>
   );
 }
